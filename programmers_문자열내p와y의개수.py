92f20e93124273b4cbe29c9f46ea8dec9406b421
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2021-01-27 00:22:58 +0900

    Delete programmers_문자열내p와y의개수.py

--- programmers_문자열내p와y의개수.py
+++
@@ -1,6 +0,0 @@
-def solution(s):    
-    return 'true' if s.upper().count('P') == s.upper().count('Y') else 'false'
-
-#다른사람 풀이
-def solution(s):
-    return s.lower().count('p') == s.lower().count('y')
